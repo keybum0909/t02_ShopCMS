c7ee22abf8acda64ee5d9cfe65b2b459c8e4e382
LAPTOP-4V9LDE6O\Wendy <yawendyshu@gmail.com>
2024-08-18 15:26:37 +0800

    [add] restrict image upload types
    - 限制圖片上傳類型

---
+++ t02_ShopCMS/t02_ShopCMS/t02_ShopCMS/Services/IProductsService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using t02_ShopCMS.Models;
+
+namespace t02_ShopCMS.Services
+{
+    public interface IProductsService
+    {
+        Task<Indexresp> Index(string searchString, string currentFilter, int? pageNumber);
+        Task<bool> Create(Product product, IFormFile myImg);
+
+
+        Task<Editresp> Edit(int? id);
+    }
+}
